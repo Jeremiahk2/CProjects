7db4208ebc6df6a9be7edd051357f2166ae96f2f
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-04 22:20:03 -0500

    implemented plus and minus functions of operation.c

---
+++ p2/operation.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operation.h"
+
+/**
+  @file operation.c
+  @author Jeremiah Knizley
+  File for performing various mathematical operations
+  such as adding, subtracting, multiplying, dividing, and exponentials
+  while detecting overflow
+*/
+
+/**
+  Adds together a and b while detecting overflow
+  @param a the number to be added with b
+  @param b the number to be added with a
+  @return long the result of the operation
+*/
+long plus( long a, long b ) 
+{
+  long result = a + b;
+  if ( result <= 0 && a > 0 && b > 0 ) {
+    exit(100);
+  }
+  if ( result >= 0 && a < 0 && b < 0 ) {
+    exit(100);
+  }
+  return result;
+}
+
+/**
+  Subtracts b from A while detecting overflow
+  @param a the number that will subtract b from itself
+  @param b the number to be subtracted from a
+  @return long the result of the operation
+*/
+long minus( long a, long b ) 
+{
+  long result = a - b;
+  if ( result >= 0 && a < 0 && b > 0 ) {
+    exit(100);
+  }
+  if ( result <= 0 && a > 0 && b < 0 ) {
+    exit(100);
+  }
+  return result;
+}
+
+/**
+  Multiplies together a and b while detecting overflow
+  @param a the number to be multiplied with b
+  @param b the number to be multiplied with a
+  @return long the result of the operation
+*/
+long times( long a, long b ) 
+{
+
+}
+
+/**
+  raises a to the power of b and returns the result while detecting overflow
+  @param a the base number to be exponentiated
+  @param b the exponent of a, must be positive
+  @return long the result of the operation
+*/
+long exponential (long a, long b )
+{
+
+}
+
+/**
+  divides a by b and returns the result while detecting overflow
+  @param a the number that will be divided by b
+  @param b the number that divides a
+  @return long the result of the operation
+*/
+long divide( long a, long b )
+{
+
+}
