389755009478f10b5217f686c7fa49ed38711bc1
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-05 15:37:12 -0500

    Implemented numbr_12.c file

---
+++ p2/number_12.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+  @file number_12.c
+  @author Jeremiah Knizley
+  The number_12.c file is responsible for parsing and printing values in base 12
+*/
+
+/**
+  Reads characters from standard input. 
+  When it reaches a character that is not whitespace or EOF,
+  it returns the code for that character and exits.
+*/
+int skip_space()
+{
+  bool charFound = false;
+  while (!charFound) {
+    char current = getchar();
+    if ( current =! ' ' ) {
+      charFound = true;
+      return (int) current;
+    }
+  }
+}
+
+long parse_value()
+{
+  long value = 0;
+  char next = getchar();
+
+  while ((next >= '0' && next <= '9') || next == 'E' || next == 'X') {
+    value = times(value, 12);
+    value = plus(value, (long) next);
+    next = getchar();
+    ungetc( next, stdin );
+  }
+  return value;
+}
+
+void print_value( long val )
+{
+  if (val == 0) {
+    putchar('0');
+    break;
+  }
+  if (val < 0) {
+    putchar('-');
+    times(val, -1);
+  }
+  long digit = val % 12;
+
+  char newChar = (char) digit;
+  if (digit == 10) {
+    newChar == 'X';
+  }
+  else if (digit == 11) {
+    newChar == 'E';
+  }
+
+  val = divide(val, 12);
+  if (val != 0) {
+    putchar( print_value(val));
+  }
+  putchar(newChar);
+}
