8a4c2b820b38eed5226dee1bcb701592d31f03f0
Jeremiah Knizley <jdknizle@ncsu.edu>
2023-02-07 01:43:37 -0500

    Added parenthesis implementation, all four EC test cases now pass.

--- p2/infix.c
+++ p2/infix.c
@@ -40,23 +40,102 @@ static long parse_mul_div()
   //default values for left and next, that way left doesn't change on the first iteration.
   long left = 1;
   char next = '*';
-
-  while (next != '+' && next != '-' && next != EOF && next != '\n') {
-    long current = parse_exp();
-    if (next == '*') {
-      left = times(current, left);
-    }
-    else if (next == '/') {
-      left = divide(left, current);
+  // char temp = skip_space();
+  // if (temp != '(') {
+  //   ungetc(temp, stdin);
+  // }
+  while (next != '+' && next != '-' && next != EOF && next != '\n' && next != ')') {
+    char temp = skip_space();
+    if (temp == '(') {
+      if (next == '*') {
+        left = times(left, parse_mul_div());
+      }
+      else if (next == '/') {
+        left = divide(left, parse_mul_div());
+      }
     }
     else {
-      exit(FAIL_INPUT);
+      ungetc(temp, stdin);
+      long current = parse_exp();
+      if (next == '*') {
+        left = times(current, left);
+      }
+      else if (next == '/') {
+        left = divide(left, current);
+      }
+      else {
+        exit(FAIL_INPUT);
+      }
     }
     next = (char) skip_space();
-    char trash = (char) skip_space();
-    ungetc(trash, stdin);
+    // char trash = (char) skip_space();
+    // ungetc(trash, stdin);
+    
   }
   ungetc(next, stdin);
+
+  return left;
+}
+/**
+  Function responsible for handling much of the parenthesis operations. 
+  Works similarly to parse_mul_div in that it goes until it finds a plus operator 
+  that is not in a parenthesis.
+  Works recursively if stacked parenthesis
+  @return value found from parsing parenthesis terms
+*/
+long paren() {
+  long left;
+  left = parse_mul_div();
+  long next;
+  char operator = (char) skip_space();
+
+  while (operator != ')') { 
+    char temp = skip_space();
+    if (temp == '(') {
+      if (operator == '+') {
+        left = plus(left, paren());
+      }
+      else if (operator == '-') {
+        left = minus(left, paren());
+      }
+    }
+    else {
+      ungetc(temp, stdin);
+      next = parse_mul_div();
+      if (operator == '+') {
+        left = plus(left, next);
+      }
+      else if (operator == '-') {
+        left = minus(left, next);
+      }
+      else {
+        exit(FAIL_INPUT);
+      }
+    }
+    operator = (char) skip_space();
+  }
+  char newOperator = skip_space();
+  if (newOperator == '*') {
+    char temp = skip_space();
+    if (temp == '(') {
+      left = times(left, paren());
+    }
+    else {
+      left = times(left, parse_mul_div());
+    }
+  }
+  else if (newOperator == '/') {
+    char temp = skip_space();
+    if (temp == '(') {
+      left = divide(left, paren());
+    }
+    else {
+      left = divide(left, parse_mul_div());
+    }
+  }
+  else {
+    ungetc(newOperator, stdin);
+  }
   return left;
 }
 
@@ -69,21 +148,44 @@ static long parse_mul_div()
 int main()
 {
   long left;
-  left = parse_mul_div();
+
+  char temp = skip_space();
+  if (temp == '(') {
+    left = paren();
+  }
+  else {
+    ungetc(temp, stdin);
+    left = parse_mul_div();
+  }
   long next;
   char operator = (char) skip_space();
-  while (operator != EOF && operator != '\n') { //for parenthesis make this a method, but make it so that it ends with ).
-    next = parse_mul_div();
-    if (operator == '+') {
-      left = plus(left, next);
-    }
-    else if (operator == '-') {
-      left = minus(left, next);
+  while (operator != EOF && operator != '\n') { 
+    char temp = skip_space();
+    if (temp == '(') {
+      if (operator == '+') {
+        left = plus(left, paren());
+      }
+      else if (operator == '-') {
+        left = minus(left, paren());
+      }
     }
     else {
-      exit(FAIL_INPUT);
+      ungetc(temp, stdin);
+      next = parse_mul_div();
+      if (operator == '+') {
+        left = plus(left, next);
+      }
+      else if (operator == '-') {
+      left = minus(left, next);
+      }
+      else {
+        exit(FAIL_INPUT);
+      }
     }
     operator = (char) skip_space();
+    while (operator == ')') {
+      operator = skip_space();
+    }
   }
   print_value(left);
   putchar('\n');
